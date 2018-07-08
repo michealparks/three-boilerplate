e6f6f72e30da49f369d2655fe79cf24653121ccb
Micheal Parks <michealparks1989@gmail.com>
2018-07-07 21:09:34 -0400

    update

--- src/util/storage.js
+++ src/util/storage.js
@@ -1,13 +1,10 @@
 export const get = (key, next) => {
   const val = JSON.parse(window.localStorage.getItem('n' + key))
-
-  console.log(key, val)
   return next !== undefined ? next(val) : val
 }
 
 export const set = (key, value, next) => {
   window.localStorage.setItem('n' + key, JSON.stringify(value))
-
   if (next !== undefined) return next()
 }
 
